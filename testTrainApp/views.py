820385f9f58bab4b5c8eac05ca8bee18fbfa6cd8
Marian Nociar <majo.nociar@gmail.com>
2020-03-03 15:54:44 +0100

    recursive train search. must be verified on a larger dataset.

--- testTrainApp/views.py
+++ testTrainApp/views.py
@@ -35,29 +35,80 @@ def departures_from_the_station(src, time):
 
 
 def stations_on_the_train_route(train, time):
-    #print(time + ">>>>>>>" + time)
-    # , departure_time__gte=time
     stops = TrainStop.objects.filter(train__number=train.train.number).order_by(
         F('departure_time').desc(nulls_first=True)
     )
     if stops[0].departure_time == None:  # Odstranime stanic ktore su pod zvolenym casom okrem None
-        #print(">>>>>>> "+str(stops.filter(departure_time=None)))
         final_stop = list(stops.filter(departure_time=None))
         stops = list(stops.filter(departure_time__gte=time))
-        print("----"+str(final_stop))
-        print("----"+str(stops))
         stops = final_stop + stops
-        print("++++" + str(stops))
-        #stops.insert(0, list(final_stop))
     else:
         stops = list(stops.filter(departure_time__gte=time))
 
-    ''' mozno bude treba dorobit aby nevracalo spoje po hladanom case
-    
-    for stop in stops:
-        if stop.departure_time == None or stop.departure_time 
-    '''
     return stops
+
+
+def search_for_connections(transfer, src, dst, time, result):
+    trains_from_station = departures_from_the_station(src, time)
+
+    trainslist = []
+    for train in trains_from_station:
+        time = train.arrival_time
+        stops = stations_on_the_train_route(train, time)
+        print(stops)
+        # je niektora zo zastavok cielova
+
+        if transfer == 3:
+            return result
+        else:
+            for stop in stops:
+                if len(result) > 0 and result[-1]['number'] == train.train.number:
+                    continue
+                if stop.station.name == src:  # vychodzie stanice sa zhoduju
+                    continue
+                if stop.station.name == dst:
+                    result.append({
+                        "number": train.train.number,
+                        "name": train.train.name,
+                        "src": src,
+                        "dst": dst
+
+                    })
+                    if isinstance(result, list):
+                        trainslist += result
+                    else:
+                        trainslist.append(result)
+                    result = []
+                    return trainslist
+
+                else:
+                    result.append({
+                        "number": train.train.number,
+                        "name": train.train.name,
+                        "src": src,
+                        "dst": stop.station.name
+                    })
+                    connect = search_for_connections(transfer + 1, stop.station.name, dst, stop.arrival_time, result)
+                    if len(connect) != 0:
+                        trainslist.append(connect) # ak je list tak len spoj
+
+                    if not bool(result[-1]):
+                        #del result[-(transfer+2):]
+                        result.pop()
+                        result.pop()
+
+                    #elif result[-1]['dst'] == dst:
+                     #   trainslist.append(result)
+                     #   result = []
+
+                    else:
+                        result = []
+                    '''
+            result.append(trainslist)
+            '''
+    if len(trains_from_station) == 0:
+        result.append({})
+    return trainslist
 
 
 class TrainResultList(APIView):
@@ -65,27 +116,19 @@ class TrainResultList(APIView):
         time = self.request.query_params.get('time')
         src = request.GET.get('src')
         dst = request.GET.get('dst')
-
+        result = []
+        result = search_for_connections(0, src, dst, time, result)
+        '''
         transfer = 0
         max_transfer = 5
 
         result = []
-        '''
-        queryset = list(TrainStop.objects.filter(train__number=810).order_by('departure_time'))
 
-        if queryset[0].departure_time == None:
-            temp = queryset.pop(0)
-            queryset.append(temp)
-        print(queryset[0])
-        print(queryset)
-        print(time + " " + src + " " + dst)
-        '''
         # zoznam vlakov odchadzajucich zo stanice v pozadovanom case
         allTrainsFromStation = departures_from_the_station(src, time)
         print(allTrainsFromStation)
         # zoznam stanic ktore konkretny vlak navstivi
         for train in allTrainsFromStation:
-            print(train.train.number)
             stops = stations_on_the_train_route(train, time)
             print(stops)
             # je niektora zo zastavok cielova
@@ -104,7 +147,7 @@ class TrainResultList(APIView):
                     break
             if len(trainslist) != 0:
                 result.append(trainslist)
-
+        '''
         return Response(result)
 
 
